d0958d0bffe04adbd14be69d6beb7777143759df
xiaofandh12 <donghao@cetc-cloud.com>
2016-06-14 19:32:41 +0800

    Algorithms Fourth Edition Chapter 5 String Count.java

---
+++ AlgorithmsFourthEdition/Chapter5_String/Count.java
@@ -0,0 +1,25 @@
+/*
+ * sudo javac-algs4 Count.java
+ * sudo java-algs4 Count ABCDR < abra.txt
+ */
+
+import edu.princeton.cs.algs4.*;
+
+public class Count
+{
+    public static void main(String[] args)
+    {
+        Alphabet alpha = new Alphabet(args[0]);
+        int R = alpha.R();
+        int[] count = new int[R];
+
+        String s = StdIn.readAll();
+        int N = s.length();
+        for (int i = 0; i < N; i++)
+            if (alpha.contains(s.charAt(i)))
+                count[alpha.toIndex(s.charAt(i))]++;
+
+        for (int c = 0; c < R; c++)
+            StdOut.println(alpha.toChar(c) + " " + count[c]);
+    }
+}
